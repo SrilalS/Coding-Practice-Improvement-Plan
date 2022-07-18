4b66c0d5a17655dcd222f731986592e2c5f18555
Srilal Sachintha <SrilalSachintha@Gmail.com>
2022-07-18 17:51:43 +0530

    initial

---
+++ Week 29/UniquePaths C#/UniquePaths/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UniquePaths
+{
+    class Program
+    {
+
+        static int UniquePathCount(int m, int n)
+        {
+            if(m == 1 || n == 1)
+            {
+                return 1;
+            } else
+            {
+                int upc = UniquePathCount(m - 1, n) + UniquePathCount(m, n - 1);
+                return upc;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter M:");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+
+            int upc = UniquePathCount(m, n);
+
+            Console.WriteLine("Number of Unique Paths : " + upc.ToString());
+        }
+    }
+}
