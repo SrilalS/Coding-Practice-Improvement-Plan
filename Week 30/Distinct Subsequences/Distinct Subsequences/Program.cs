947b91584c2519415d16daf3272bf71a3f0d88e3
Srilal Sachintha <SrilalSachintha@Gmail.com>
2022-07-29 12:39:38 +0530

    init

---
+++ Week 30/Distinct Subsequences/Distinct Subsequences/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Distinct_Subsequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
